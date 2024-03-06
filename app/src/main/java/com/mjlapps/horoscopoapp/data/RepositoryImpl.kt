bc8a4751a3c4ef5b1928cce13538ec996f2300f2
mjlascar <marcosjlascar@gmail.com>
2024-03-06 11:21:14 -0300

    add data folder tied w api sevice

--- app/src/main/java/com/mjlapps/horoscopoapp/data/RepositoryImpl.kt
+++ app/src/main/java/com/mjlapps/horoscopoapp/data/RepositoryImpl.kt
@@ -1,12 +1,19 @@
 package com.mjlapps.horoscopoapp.data
 
+import android.util.Log
 import com.mjlapps.horoscopoapp.data.network.HoroscopeApiService
+import com.mjlapps.horoscopoapp.data.network.response.PredictionResponse
 import com.mjlapps.horoscopoapp.domain.Repository
+import com.mjlapps.horoscopoapp.domain.model.PredictionModel
 import retrofit2.Retrofit
 import javax.inject.Inject
 
-class RepositoryImpl @Inject constructor(private val apiService: HoroscopeApiService): Repository {
-    override suspend fun getPrediction(sign: String) {
-        //Ask Api prediction via retrofit
+class RepositoryImpl @Inject constructor(private val apiService: HoroscopeApiService) : Repository {
+    override suspend fun getPrediction(sign: String): PredictionModel? {
+        runCatching { apiService.getHoroscope(sign) }
+            .onSuccess { return it.toDomain() } //converted it from data PredictionResponse to domain PredictionModel
+            .onFailure { Log.i("oof", "an error has ocurred ${it.message}") }
+
+        return null
     }
 }
--- app/src/main/java/com/mjlapps/horoscopoapp/domain/Repository.kt
+++ app/src/main/java/com/mjlapps/horoscopoapp/domain/Repository.kt
@@ -1,5 +1,8 @@
 package com.mjlapps.horoscopoapp.domain
 
+import com.mjlapps.horoscopoapp.data.network.response.PredictionResponse
+import com.mjlapps.horoscopoapp.domain.model.PredictionModel
+
 interface Repository {
-    suspend fun getPrediction(sign: String)
+    suspend fun getPrediction(sign: String): PredictionModel?
 }
